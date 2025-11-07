de06d701362f45eb3e1073669033e7339962b76c
Sara Kangaslahti <56774450+skangasl@users.noreply.github.com>
2025-11-07 16:44:13 -0500

    Implement layer cutoff checks for pythia.py
    Add early return for specific layer conditions in pythia.py

--- patching/pythia.py
+++ patching/pythia.py
@@ -34,6 +34,9 @@ def patch_first_k_layers(
     student_idx, teacher_idx = get_layer_cutoffs(
         student_config["copied_teacher_layers"], k, patch_from_last=False
     )
+    if student_idx == 0:
+        return teacher_model
+        
     intermediate_model = reload_student_model(
         student_model,
         teacher_model,
@@ -83,6 +86,9 @@ def patch_last_k_layers(
     student_idx, teacher_idx = get_layer_cutoffs(
         student_config["copied_teacher_layers"], k, patch_from_last=True
     )
+    if student_idx == student_model.config.num_hidden_layers:
+        return teacher_model
+        
     intermediate_model = reload_student_model(
         student_model,
         teacher_model,
